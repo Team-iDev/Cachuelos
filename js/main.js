22f0f6b7b32d777bc4f7bdfd70a94e893b0e5719
Marco Altamirano <maaltamiranohiga@gmail.com>
2019-10-02 23:10:06 -0500

    Fixing header
    Fixing what the header do when someone scrolls

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var btnRegister = document.getElementById("btnRegister");
 var bgLogin = document.getElementById("infoLogin").parentNode;
 console.log(bgLogin)
 var btnLogin = document.getElementById("btnLogin");
-
 function toggleElement(e) {
 
 	if( e.style.visibility != 'visible' ) {
@@ -25,7 +24,15 @@ function showElement(e) {
 	e.style.visibility = 'visible'
 }
 
-
+var html = document.documentElement;
+	
+window.onscroll = function(){
+	if(html.scrollTop > 200){
+		document.getElementById("nav").style.background="#1f618d";
+	} else {
+		document.getElementById("nav").style.background="none";
+	}
+}
 bgRegister.addEventListener("click", () => { toggleElement( bgRegister ) });
 btnRegister.addEventListener("click", () => { toggleElement( bgRegister ) });
 bgLogin.addEventListener("click", () => { toggleElement( bgLogin ) });
