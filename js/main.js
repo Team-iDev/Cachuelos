7a2609eced895a6f018aa06f19c805b7fdd378a3
Marco Altamirano <maaltamiranohiga@gmail.com>
2019-10-10 21:24:52 -0500

    casi terminado el menumobile
    Falta el evento click para cerrar el menu mobile

--- js/main.js
+++ js/main.js
@@ -1,41 +1,55 @@
+/*MODEL BOXES(REGISTER,LOGIN)*/
 var bgRegister = document.getElementById("infoRegister").parentNode;
-console.log(bgRegister)
 var btnRegister = document.getElementById("btnRegister");
 var bgLogin = document.getElementById("infoLogin").parentNode;
-console.log(bgLogin)
 var btnLogin = document.getElementById("btnLogin");
-function toggleElement(e) {
-
-	if( e.style.visibility != 'visible' ) {
-		showElement(e);
-		return;
-	} else {
-		hideElement(e);
-		return;
+	function hideElement(e) {
+		e.style.visibility = 'hidden';
 	}
-
-}
-
-function hideElement(e) {
-	e.style.visibility = 'hidden';
-}
-
-function showElement(e) {
-	e.style.visibility = 'visible'
-}
-
-var html = document.documentElement;
-	
-window.onscroll = function(){
-	if(html.scrollTop > 200){
-		document.getElementById("nav").style.background="#1f618d";
-	} else {
-		document.getElementById("nav").style.background="none";
+	function showElement(e) {
+		e.style.visibility = 'visible';
 	}
-}
+	function toggleElement(e) {
+		if( e.style.visibility != 'visible' ) {
+			showElement(e);
+			return;
+		} else {
+			hideElement(e);
+			return;
+		}
+	}
 bgRegister.addEventListener("click", () => { toggleElement( bgRegister ) });
 btnRegister.addEventListener("click", () => { toggleElement( bgRegister ) });
 bgLogin.addEventListener("click", () => { toggleElement( bgLogin ) });
 btnLogin.addEventListener("click", () => { toggleElement( bgLogin ) });
+/*HEADER ONSCROLL*/
+var html = document.documentElement;
+	window.onscroll = function(){
+		if(html.scrollTop > 200){
+			document.getElementById("nav").style.background="#1f618d";
+		} else {
+			document.getElementById("nav").style.background="none";
+		}
+	}
+/*MENU RESPONSIVE*/
+var mobilemenu = document.getElementById("mobilemenu");
+var ulMobileMenu = document.getElementById("ulMobileMenu");
+var menu_bars = document.getElementById("menu_bars");
+var boxLogo = document.getElementById("boxLogo");
+var navbar = document.getElementById("navbar");
+var liIniciarSesion = document.getElementById("liIniciarSesion");
+var liRegistrarse = document.getElementById("liRegistrarse");
+var closeMobileMenu = document.getElementById("closeMobileMenu");
+menu_bars.addEventListener("click",function(){
+	mobilemenu.classList.remove("hidden");
+	menu_bars.classList.add("hidden");
+	boxLogo.classList.add("hidden");
+	ulMobileMenu.classList.add("col");
+	navbar.classList.add("menu_mobile");
+	liIniciarSesion.classList.remove("hidden");
+	liRegistrarse.classList.remove("hidden");
+	closeMobileMenu.classList.remove("hidden");
+});
+closeMobileMenu.addEventListener("click",toggleElement(mobilemenu));
 
 
